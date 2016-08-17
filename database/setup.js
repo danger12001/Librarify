ccd35aec7cd44772e33fca67db3a74f16a8c444a
ntombi20 <ntombi@projectcodex.co>
2016-08-17 15:42:07 +0200

    Ntombi: done creating with all the view pages

--- database/setup.js
+++ database/setup.js
@@ -13,7 +13,7 @@ var adminInsert = String(fs.readFileSync('./database/sql/admin.sql'));
   var dbOptions = {
     host: '127.0.0.1',
     user: 'root',
-    password: 'mxmaolqk',
+    password: '12345',
     port: 3306,
     database: "librarifyDB"
   };
