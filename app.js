a76a2faec7349ee4f3ff9cfca09c0e53460e2459
danger12001 <paul@projectcodex.co>
2016-08-18 16:28:40 +0200

    fixed pw

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ PicDataService = require('./data-services/picDataService');
     var dbOptions = {
       host: '127.0.0.1',
       user: 'root',
-      password: '12345',
+      password: 'password1!',
       port: 3306,
       database: "librarifyDB"
     };
